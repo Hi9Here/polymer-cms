89f45232c2f5b45127fab77cff1534bff17721d4
Wo <mywoisme@gmail.com>
2015-02-07 18:31:58 +0000

    first working example

--- app/app.js
+++ app/app.js
@@ -1,10 +1,3 @@
-document.addEventListener('polymer-ready', function() {
-  var navicon = document.getElementById('navicon');
-  var drawerPanel = document.getElementById('drawerPanel');
-  navicon.addEventListener('click', function() {
-    drawerPanel.togglePanel();
-  });
-
 var tabs = document.querySelector('paper-tabs');
 
 /* Add the following code: */
