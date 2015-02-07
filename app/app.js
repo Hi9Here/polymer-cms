89eb0d1808e361dee01647f0c9f4ae2d4afb43f5
Wo <mywoisme@gmail.com>
2015-02-07 18:25:27 +0000

    Menu working but not perfect

--- app/app.js
+++ app/app.js
@@ -1,3 +1,10 @@
+document.addEventListener('polymer-ready', function() {
+  var navicon = document.getElementById('navicon');
+  var drawerPanel = document.getElementById('drawerPanel');
+  navicon.addEventListener('click', function() {
+    drawerPanel.togglePanel();
+  });
+
 var tabs = document.querySelector('paper-tabs');
 
 /* Add the following code: */
@@ -5,4 +12,7 @@ var list = document.querySelector('post-list');
 
 tabs.addEventListener('core-select', function() {
   list.show = tabs.selected;
+});
+
+
 });
